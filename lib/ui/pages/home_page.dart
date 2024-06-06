6d769c2878839031d0e7449d627539a6c0b71a7d
NesmaAlabyad <92161296+NesmaAlabyad@users.noreply.github.com>
2024-06-06 11:31:51 +0300

    task
    my task

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('News App'),
+        title: Text('News Appp'),
         centerTitle: true,
       ),
       body: BlocBuilder<NewsBloc, NewsState>(
